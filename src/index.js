7faa48094e5dabbc564db6343dfd99c0c55865ec
humors2025 <humorstech22@gmail.com>
2026-04-24 18:15:10 +0530

    checking ci/cd

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const apiRoutes = require("./routes/apiRoutes");
 
 const app = express();
 
-console.log("🔥 Lambda function gets HIT ");
+console.log("🔥 Lambda function gets HIT done ");
 
 // =====================================================
 // ✅ CONDITIONAL CORS MIDDLEWARE (Works for both local and AWS)
